aef9053c7f16a5d0e13a699cf113011809d9f48f
Mark Hummel <mdh@raquette.com>
2016-04-26 12:58:50 -0400

    initial import

---
+++ wait.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Waiting for mysql"
+until mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -uroot -p"$MYSQL_PASSWORD" &> /dev/null
+do
+  printf "."
+  sleep 1
+done
+
+echo -e "\nmysql ready"
+
